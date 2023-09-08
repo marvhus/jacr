51b0d96365eb5dde51a2179f6e933b0f8c655dd4
Martin <marvahus@proton.me>
2023-09-08 20:53:26 +0200

    Implemented rot13, and caesar
    + Implemented build script search for ciphers
    + Implented rot13
    + Implemented caesar
    + Fixed mistakes from template

--- build_and_run.sh
+++ build_and_run.sh
@@ -7,7 +7,7 @@ set -e
 cd "$(dirname "$(readlink -f "$0")")"
 
 echo '===== BUILD ====='
-jai first.jai -import_dir modules
+jai build.jai -import_dir modules
 echo '====== RUN ======'
 ./bin/exec
 echo '================='
